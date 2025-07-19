edbf532eaf02490c17128b3f15920e0147b6ec05
guilherme-alc <guilhermealc01@gmail.com>
2025-07-19 11:28:47 -0300

    Altera para TryAdd

--- Extensions/DependenciesExtension.cs
+++ Extensions/DependenciesExtension.cs
@@ -3,6 +3,7 @@ using DependencyStore.Repositories.Interfaces;
 using DependencyStore.Services;
 using DependencyStore.Services.Interfaces;
 using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace DependencyStore.Extensions
 {
@@ -16,17 +17,17 @@ namespace DependencyStore.Extensions
 
         public static void AddRepositories(this IServiceCollection services)
         {
-            services.AddTransient<ICustomerRepository, CustomerRepository>();
-            services.AddScoped<IProductRepository, ProductRepository>();
-            services.AddTransient<IPromoCodeRepository, PromoCodeRepository>();
+            services.TryAddTransient<ICustomerRepository, CustomerRepository>();
+            services.TryAddScoped<IProductRepository, ProductRepository>();
+            services.TryAddTransient<IPromoCodeRepository, PromoCodeRepository>();
         }
 
         public static void AddServices(this IServiceCollection services)
         {
-            services.AddTransient<IDeliveryService, DeliveryService>();
-            services.AddScoped<ICalculateOrderService, CalculateOrderService>();
-            services.AddScoped<IProductService, ProductService>();
-            services.AddScoped<IOrderService, OrderService>();
+            services.TryAddTransient<IDeliveryService, DeliveryService>();
+            services.TryAddScoped<ICalculateOrderService, CalculateOrderService>();
+            services.TryAddScoped<IProductService, ProductService>();
+            services.TryAddScoped<IOrderService, OrderService>();
         }
     }
 }
