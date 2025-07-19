da435aaf151ae4b001136b27cc69902e1bf7ce57
guilherme-alc <guilhermealc01@gmail.com>
2025-07-19 09:40:14 -0300

    Injeção de dependencias no program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,25 @@
+using DependencyStore.Repositories;
+using DependencyStore.Repositories.Interfaces;
+using DependencyStore.Services;
+using DependencyStore.Services.Interfaces;
+using Microsoft.Data.SqlClient;
+
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddControllers();
+
+builder.Services.AddHttpClient();
+
+builder.Services.AddScoped(provider =>
+    new SqlConnection("CONN_STRING"));
+
+builder.Services.AddTransient<ICustomerRepository, CustomerRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddTransient<IPromoCodeRepository, PromoCodeRepository>();
+builder.Services.AddTransient<IDeliveryService, DeliveryService>();
+builder.Services.AddScoped<ICalculateOrderService, CalculateOrderService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+
 
 var app = builder.Build();
 
